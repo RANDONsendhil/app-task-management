4d5e2ffe111f6e4bb995199b33bb3fe1c7a5e4f3
Randon <randon2006@gmail.com>
2025-02-13 13:02:54 +0100

    connected user to db for user creation

--- homeComponent/view/home.php
+++ homeComponent/view/home.php
@@ -1,21 +1,66 @@
   <style>
+    .card {
+      display: flex;
+      padding: 20px;
+      width: 800px;
+      flex-direction: row;
+      gap: 61px;
+      margin-left: 10px;
+      flex-direction: row;
+    }
 
+    .card-body {
+      margin: 20px;
+    }
   </style>
 
   <?php
   include(BASE_PATH . "/index/navHome.php")
   ?>
-
   <!-- Main content (for demonstration) -->
   <div class="content-home" style="margin-left: 250px;">
-
-
     <?php
-
     include(BASE_PATH . "/index/headerHome.php")
-
     ?>
 
     <h1>Main Content Area</h1>
-    <p>Here you can add the content that will be shown alongside the sidebar.</p>
+
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+      <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+      <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+      <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+      <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+      <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+      <div class="card-body">
+        <h5 class="card-title">Special title treatment</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+
+      </div>
+    </div>
   </div>
